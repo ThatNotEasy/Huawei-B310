b68d4736a674f1d14c675a2bfc53556fe4dcb714
ThatNotEasy <apid9755@gmail.com>
2024-01-18 04:35:26 +0800

    Update aio.sh

--- aio.sh
+++ aio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Remote SSH command to create a tar archive
-ssh -p 22 root@192.168.8.1 "cd /online/AIO_LoveTacome/GUI && tar czf archive.tar.gz *"
+ssh -p 22 root@192.168.8.1 "cd /app/webroot/WebApp/common/ && tar czf meowing.tar.gz *"
 
 # SCP to copy the archive to the local machine (adjust the destination path as needed)
-scp -P 22 root@192.168.8.1:/online/AIO_LoveTacome/GUI/archive.tar.gz /path/to/local/directory
+scp -P 22 root@192.168.8.1:/app/webroot/WebApp/common/meowing.tar.gz /Desktop
